b073c0a00db81c02d3b531b06b3c3f6f1e58a21c
Gabriela Shaooli <0245501@up.edu.mx>
2025-09-09 14:02:26 -0600

    chore: ignore .adminjs build dir

--- .adminjs/entry.js
+++
@@ -1,5 +0,0 @@
-AdminJS.UserComponents = {}
-import MediaPreview from '../components/MediaPreview'
-AdminJS.UserComponents.MediaPreview = MediaPreview
-import WelcomeDashboard from '../components/WelcomeDashboard'
-AdminJS.UserComponents.WelcomeDashboard = WelcomeDashboard
